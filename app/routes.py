73eb36d953c66378b6b86af3dba78c8f877c99ec
blith <garciwi@oregonstate.edu>
2021-06-09 21:58:01 -0500

    after adding api_key

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,18 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 from app import app
 from app.forms import SearchForm
 
 @app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET'])
 def index():
     user = {'username':'Will'}
     form = SearchForm()
     return render_template('index.html', title='Home', form=form)
 
+@app.route('/search', methods=["GET","POST"])
+def search_result():
+    text =request.form['text']
+    text_two = text.split()
+    processed = "+".join(text)
+    to_be_returned_from_OMDB ="https://www.omdbapi.com/?s="+processed+"&apikey=e3c04726"
 
