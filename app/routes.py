73aa07b8b4ac197cc42f5d3a766a95066e5b7779
blith <garciwi@oregonstate.edu>
2021-06-10 12:57:54 -0500

    to new branch

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,16 @@ from app import app
 from app.forms import SearchForm
 
 @app.route('/')
-@app.route('/index', methods=['GET'])
+@app.route('/index') 
 def index():
     user = {'username':'Will'}
     form = SearchForm()
     return render_template('index.html', title='Home', form=form)
 
-@app.route('/search', methods=["GET","POST"])
+@app.route('/search', methods=["POST"])
 def search_result():
     text =request.form['text']
     text_two = text.split()
     processed = "+".join(text)
     to_be_returned_from_OMDB ="https://www.omdbapi.com/?s="+processed+"&apikey=e3c04726"
-
+    return to_be_returned_from_OMDB
