616263f81209ae28ea999c16bb4887ed04608c72
blith <garciwi@oregonstate.edu>
2021-06-19 17:15:10 -0500

    improved modularity

--- app/routes.py
+++ app/routes.py
@@ -24,26 +24,18 @@ def search():
     # receives the incoming POST request from the form and turns it into text
     text =request.form['text']
     # processes the text into a string format to add to the API query string
-    text_two = text.split()
-    processed = "+".join(text_two)
-    to_be_returned_from_OMDB ="https://www.omdbapi.com/?s="+processed+"&page=1&apikey=e3c04726"
-    # uses the 'urllib.request as pull' to open the url as a response and then open as a dictionary with data
-    with pull.urlopen(to_be_returned_from_OMDB) as response:
-        source = response.read()
-        data = json.loads(source)
-    search_list =  data["Search"]
-    for movie in search_list:
-        result1 = result(imdbID=movie["imdbID"], title=movie["Title"], year=movie["Year"])
-        db.session.add(result1)
-        db.session.commit()
-    return render_template('search.html', title="Results", results=search_list, form=form)
+    results = _return_json(text)
+    if results is False:
+        flash("No results found, please enter new search terms")
+        return redirect("/index")
+    return render_template('search.html', title="Results", results=results, form=form)
 
 @app.route('/details', methods=["POST"])
 def details():
     text_id = request.form['imdbID']
     to_be_returned_from_OMDB ="https://www.omdbapi.com/?i="+text_id+"&apikey=e3c04726"
 
-def _add_to_db(text):
+def _return_json(text):
     """function for addiing json object results to db depending on result amount and returning a SQLAlchemy object"""
     # starts of by getting the url for the first page of results 
     url_string = _string_to_url(text, 1)
@@ -54,28 +46,21 @@ def _add_to_db(text):
     
     if data["Response"] == "False":
         # if the json object found no results, then redirect back to the home search screen
-        flash("No results, please enter new search terms")
-        return redirect("/index")
+        return False 
     # otherwise, if response is at least one ...
     else:
-        search_list = None
-        if data["totalResults"] <= 10:
-            for movie in search_list:
-                result1 = result(imdbID=movie["imdbID"], title=movie["Title"], year=movie["Year"])
-                db.session.add(result1)
-                db.session.commit()
-            search_list = db.session.query(result).all()
-        # otherwise, data["totalResults"] > 10
-        else:
+        total_results = int(data["totalResults"])
+        total_pages = total_results // 10
+        if total_pages >= 5:
             for i in range(1,6):
-                if data["totalResults"] < 20:
-                    # change_url by passing to _string_to_url for text, num = 2)
-                    # add to db
-                elif data["totalResults"] < 30:
-                    # change_url by passing to _string_to_url for text, num = 2)
-                    # add to db
-                # ....and so on until we return the JSON object, DO WE NEED TO RETURN A JSON OBJECT?
-
+                url_string = _string_to_url(text,i)
+                _add_to_db(url_string)
+            return db.session.query(result).all()
+        else:
+            for i in range(1,total_pages + 1):
+                url_string = _string_to_url(text,i)
+                _add_to_db(url_string)
+            return db.session.query(result).all()
 
 def _string_to_url(text, num):
     """function for converting text to a url for omdb api
@@ -83,5 +68,18 @@ def _string_to_url(text, num):
     param: specific page number of total results to return"""
     text_two = text.split()
     processed = "+".join(text_two)
-    to_be_returned_from_OMDB ="https://www.omdbapi.com/?s="+processed+"&page="+num+"&apikey=e3c04726"
+    to_be_returned_from_OMDB ="https://www.omdbapi.com/?s="+processed+"&page="+str(num)+"&apikey=e3c04726"
     return to_be_returned_from_OMDB
+
+def _add_to_db(url_string):
+    """ helper function for _return_json function that adds json object results to database (db)"""
+
+    # uses the 'urllib.request as pull' to open the url as a response and then open as a dictionary with data
+    with pull.urlopen(url_string) as response:
+        source = response.read()
+        data = json.loads(source)
+    search_list = data["Search"]
+    for movie in search_list:
+        result1 = result(imdbID=movie["imdbID"], title=movie["Title"], year=movie["Year"])
+        db.session.add(result1)
+        db.session.commit()
